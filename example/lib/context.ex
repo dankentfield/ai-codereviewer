f3375d32f0200ac20fb0fc306b8256fa799a2a36
Daniel Kentfield <danielkent03@gmail.com>
2024-11-28 10:19:43 +0000

    more changes

--- example/lib/context.ex
+++ example/lib/context.ex
@@ -15,13 +15,16 @@ defmodule App.Context do
     end
   end
 
-  def hello_world(colour) do
+  def hello_colour(colour) do
     if colour == :red do
       IO.puts("Hello, red")
-    else if colour == :blue do
+    elseif colour == :blue do
       IO.puts("Hello, blue")
+    elseif colour == :green do
+      IO.puts("Hello, green")
     else
       IO.puts("Hello, world!")
     end
   end
 end
+
